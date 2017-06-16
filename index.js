f3484b1f6af1017cdad9874e7f2d46c9fd62ea0d
Harold Luo <luozh@shinemo.com>
2017-06-16 20:24:04 +0800

    feat(update):

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 module.exports = {
-    blocks: {
-
-    },
-
     hooks: {
         "page:before": function(page) {
-            console.log(page);
-            if(page!=undefined && ((page.type == undefined && page.path.test(/adoc$/)) || page.type === "asciidoc")){
-                page.content = page.content.replace(/include::(.+)\[.*\]/g,"{% include \"$1\" %}");
+            console.log('%s,%s','page1',page);
+            if(page != undefined && ((page.path.test(/\.adoc$/) || page.path.test(/\.asciidoc$/)) || page.type === 'asciidoc')){
+                page.content = page.content.replace(/include::(.+)\[.*\]/g,'{% include \"$1\" %}');
             }
-            console.log(page);
+            console.log('%s,%s','page2',page);
             return page;
         }
     }
