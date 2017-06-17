0d41aff4deabeba196285f24c29530cac983cceb
ZihoRo <ZihoRo@zihoro.com>
2017-06-17 18:49:56 -0400

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
     hooks: {
         "page:before": function(page) {
-            var debug = this.output ? this.output.logger.debug.ln : console.log;
-            debug('page:before type:' + (!page || page.type) + ',path:' + (!page || page.path) + ',page:' + page);
+            // var debug = this.output ? this.output.logger.debug.ln : console.log;
+            console.log('page:before type:' + (!page || page.type) + ',path:' + (!page || page.path) + ',page:' + page);
             if(page != undefined && ((page.path.test(/\.adoc$/) || page.path.test(/\.asciidoc$/)) || page.type === 'asciidoc')){
                 page.content = page.content.replace(/include::(.+)\[.*\]/g,'{% include \"$1\" %}');
             }
-            debug('page:before page:' + page);
+            console.log('page:before page:' + page);
             return page;
         }
     }
