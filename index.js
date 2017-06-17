e6dba931be72a0497c602c29c5148d665b7396f5
ZihoRo <ZihoRo@zihoro.com>
2017-06-17 18:41:26 -0400

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
     hooks: {
         "page:before": function(page) {
-            var logger = this.output.getLogger();
-            logger.debug.ln('page:before type:' + (!page || page.type) + ',path:' + (!page || page.path) + ',page:' + page);
+            var debug = this.output ? this.output.getLogger().debug.ln : console.log;
+            debug('page:before type:' + (!page || page.type) + ',path:' + (!page || page.path) + ',page:' + page);
             if(page != undefined && ((page.path.test(/\.adoc$/) || page.path.test(/\.asciidoc$/)) || page.type === 'asciidoc')){
                 page.content = page.content.replace(/include::(.+)\[.*\]/g,'{% include \"$1\" %}');
             }
-            logger.debug.ln('page:before page:' + page);
+            debug('page:before page:' + page);
             return page;
         }
     }
