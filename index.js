60f9ca00044153418769526c6fc70cc1b756ec58
ZihoRo <ZihoRo@zihoro.com>
2017-06-17 18:47:31 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
     hooks: {
         "page:before": function(page) {
-            var debug = this.output ? this.output.getLogger().debug.ln : console.log;
+            var debug = this.output ? this.output.logger.debug.ln : console.log;
             debug('page:before type:' + (!page || page.type) + ',path:' + (!page || page.path) + ',page:' + page);
             if(page != undefined && ((page.path.test(/\.adoc$/) || page.path.test(/\.asciidoc$/)) || page.type === 'asciidoc')){
                 page.content = page.content.replace(/include::(.+)\[.*\]/g,'{% include \"$1\" %}');
