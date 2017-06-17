fc9cd7ca6dac91dee60f25d37fd5779fa1c781c6
ZihoRo <ZihoRo@zihoro.com>
2017-06-17 18:37:37 -0400

    update

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-require('gitbook-core')
-
 module.exports = {
     hooks: {
         "page:before": function(page) {
             var logger = this.output.getLogger();
-            logger.debug.ln('page:before:pre :' + page);
+            logger.debug.ln('page:before type:' + (!page || page.type) + ',path:' + (!page || page.path) + ',page:' + page);
             if(page != undefined && ((page.path.test(/\.adoc$/) || page.path.test(/\.asciidoc$/)) || page.type === 'asciidoc')){
                 page.content = page.content.replace(/include::(.+)\[.*\]/g,'{% include \"$1\" %}');
             }
-            logger.debug.ln('page:before:post :' + page);
+            logger.debug.ln('page:before page:' + page);
             return page;
         }
     }
