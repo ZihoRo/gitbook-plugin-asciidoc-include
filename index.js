858e415a70ed552796e94a3696a18295f109338d
ZihoRo <ZihoRo@zihoro.com>
2017-06-18 00:41:08 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var path = require('path');
 var fs = require('fs');
-var Q = require('q');
-var is = require('is');
 
 module.exports = {
     hooks: {
@@ -15,40 +13,17 @@ module.exports = {
             var charset = this.config.get('charset', 'UTF-8');
 
             var dir = path.dirname(page.rawPath);
-            var joinPath = function(filename) {
-                return path.join(dir, filename);
-            };
 
             // find all include::url[] statements.
             var pattern = 'include::(.+)\\[.*\\]';
             var regex = new RegExp(pattern, 'ig');
             var matchs = regex.exec(page.content);
-            var replaceHandlerArray = [];
-            var match;
-            var file;
             while (matchs) {
-              match = matchs[0];
-              file = matchs[1];
               logger.info.ln(match + ',' + file);
-              replaceHandlerArray.push(Q.nfcall(fs.readFile, joinPath(file), charset).then(function(text) {
-                      return text.toString().trim();
-                  }).then(function(text) {
-                      if(!(!text || text.toString().trim() === '')){
-                          logger.debug.ln('replace:true');
-                          page.content = page.content.replace(match, text);
-                      }
-                      return page;
-                  })
-              );
+              page.content = page.content.replace(matchs[0], fs.readFileSync(path.join(dir, matchs[1]), charset).trim());
               matchs = regex.exec(page.content);
             }
-            return Q.all(replaceHandlerArray).then(function(result){
-                if(is.not.array(result) || result.length == 0){
-                    return page;
-                }
-                logger.info.ln(result);
-                return result[result.length - 1];
-            });
+            return page;
         }
     }
 };
